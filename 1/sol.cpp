9eb23062c415296662fb773c1d10e02a245e1222
Daniel Woldegiorgis <dww2@williams.edu>
2018-11-21 12:36:55 -0500

    Life, the universe, and everything

---
+++ 1/sol.cpp
@@ -0,0 +1,22 @@
+/*
+ Author: Daniel Woldegiorgis
+ Date: 9/21/2018
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int input;
+    while(cin >> input && input != 42){
+        cout << input << "\n";
+    }
+    return 0;
+}
+
+
+
+
+
+
